171924633fb845a1ed28ee0eff7d41be47d667f0
jieddev <jiedelflorentino.ruizo@gmail.com>
2025-09-12 22:45:16 +0800

    Add highlight animation effect on Button Tap

--- lib/components/button_custom.dart
+++ lib/components/button_custom.dart
@@ -1,20 +1,49 @@
 import 'package:flutter/material.dart';
 
-class ButtonCustom extends StatelessWidget {
-  final void Function()? onTap;
+class ButtonCustom extends StatefulWidget {
+  final VoidCallback? onTap;
   final String text;
 
   const ButtonCustom({super.key, required this.onTap, required this.text});
 
   @override
+  State<ButtonCustom> createState() => _ButtonCustomState();
+}
+
+class _ButtonCustomState extends State<ButtonCustom> {
+  bool _isPressed = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() => _isPressed = true);
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() => _isPressed = false);
+  }
+
+  void _handleTapCancel() {
+    setState(() => _isPressed = false);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        padding: EdgeInsets.all(25),
-        decoration: BoxDecoration(color: Colors.black),
+      onTap: widget.onTap,
+      onTapDown: _handleTapDown,
+      onTapUp: _handleTapUp,
+      onTapCancel: _handleTapCancel,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 150), // Smooth transition
+        padding: const EdgeInsets.all(25),
+        decoration: BoxDecoration(
+          color: _isPressed ? Colors.grey[800] : Colors.black,
+          borderRadius: BorderRadius.circular(8),
+        ),
         child: Center(
-          child: Text(text, style: TextStyle(color: Colors.white)),
+          child: Text(
+            widget.text,
+            style: const TextStyle(color: Colors.white, fontSize: 16),
+          ),
         ),
       ),
     );
